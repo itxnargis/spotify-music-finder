d492aabc237ea0948b80b608e0039e3d3a5d1ee3
itxnargis <itxnargiskhatun@gmail.com>
2024-12-05 19:18:08 +0530

    Fix: React error

--- src/components/AudioUploader.tsx
+++ src/components/AudioUploader.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import { useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
 import { CloudUploadIcon } from 'lucide-react'
 import toast from 'react-hot-toast'
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { MusicIcon } from 'lucide-react'
 
 export function Header() {
